3c037d2d32ddc5d1b737f28f330eccdfe0dd35be
id2k1149 <id2k1149@gmail.com>
2022-12-01 18:55:46 -0500

    Initial Commit

---
+++ SushiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SushiApp
+//
+//  Created by Max Franz Immelmann on 12/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
